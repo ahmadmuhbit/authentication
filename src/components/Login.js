91a16c4ffe1f4d6e4b926a27ffd3e779ad5c330e
ahmadmuhbit <ahmadmuhbit@gmail.com>
2020-06-26 10:16:56 +0700

    onChange Form Input Login Username n Password React JS

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
  
 const Login = () => {
+    const[username,setUsername] = useState('');
+    const[password,setPassword] = useState('');
+
+    const onChangeUsername = (e) => {
+        const value = e.target.value
+        setUsername(value)
+    }
+
+    const onChangePassword = (e) => {
+        const value = e.target.value
+        setPassword(value)
+    }
 
     return (
         <div style={{marginTop: "230px"}}>
@@ -11,12 +23,12 @@ const Login = () => {
                             <div className="card-body">
                                 <div className="form-group">
                                     <label>Username</label>
-                                    <input type="text" placeholder="Username" className="form-control"></input>
+                                    <input type="text" placeholder="Username" className="form-control" value={username} onChange={onChangeUsername} />
                                 </div>
 
                                 <div className="form-group">
                                     <label>Password</label>
-                                    <input type="password" placeholder="Password" className="form-control"></input>
+                                    <input type="password" placeholder="Password" className="form-control" value={password} onChange={onChangePassword} />
                                 </div>
 
                                 <button className="btn btn-primary">Login</button>
