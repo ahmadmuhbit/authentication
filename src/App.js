38e7d0960676ed643e2c393f8d1499db0108ae79
ahmadmuhbit <ahmadmuhbit@gmail.com>
2020-06-26 10:54:24 +0700

    Form Input Untuk Halaman Daftar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './components/Login';
+import Daftar from './components/Daftar';
 
 function App() {
   return (
     <div className="App">
-        <Login />
+        {/* <Login /> */}
+        <Daftar />
     </div>
   );
 }
